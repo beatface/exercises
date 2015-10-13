97c89a05cb4131f4ab886572d2fe8125ad196dd5
Emma Beaton <emma.beaton@gmail.com>
2015-10-13 11:53:53 -0500

    added grades.js

---
+++ grades/grades.js
@@ -0,0 +1,85 @@
+var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
+
+
+for (var i; i < scores.length; i++) {
+	var score = scores[i];
+	switch (score) {
+		case 50:
+		case 51:
+		case 52:
+		case 53:
+		case 54:
+		case 55:
+		case 56:
+		case 57:
+		case 58:
+		case 59:
+		case 60:
+			console.log("F");
+			break;
+		case 61:
+		case 62:
+		case 63:
+		case 64:
+		case 65:
+		case 66:
+		case 67:
+		case 68:
+		case 69:
+		case 70:
+			console.log("D");
+			break;
+		case 71:
+		case 72:
+		case 73:
+		case 74:
+		case 75:
+		case 76:
+		case 77:
+		case 78:
+		case 79:
+		case 80:
+			console.log("C");
+			break;
+		case 81:
+		case 82:
+		case 83:
+		case 84:
+		case 85:
+		case 86:
+		case 87:
+		case 88:
+		case 89:
+		case 90:
+			console.log("B");
+			break;
+		case 91:
+		case 92:
+		case 93:
+		case 94:
+		case 95:
+		case 96:
+		case 97:
+		case 98:
+		case 99:
+		case 100:
+			console.log("A");
+			break;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

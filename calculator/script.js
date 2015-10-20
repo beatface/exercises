26e56eac4a7ecce236fd57b23892a58900fe2f75
Emma Beaton <emma.beaton@gmail.com>
2015-10-20 15:05:43 -0500

    finished challenge

---
+++ calculator/script.js
@@ -0,0 +1,95 @@
+var firstInput = document.getElementById("num-input-1");
+var secondInput = document.getElementById("num-input-2");
+var addButton = document.getElementById("add");
+var subtractButton = document.getElementById("subtract");
+var multiplyButton = document.getElementById("multiply");
+var divideButton = document.getElementById("divide");
+var resultDiv = document.getElementById("result-container");
+
+
+/*
+  Create a function that multiplies two numbers
+  passed in as arguments. Return the product.
+ */
+function multiplyNum(num1, num2) {
+  return num1 * num2;
+}
+
+
+/*
+  Create a function that adds two numbers
+  passed in as arguments. Return the sum.
+ */
+function addNum(num1, num2) {
+  return num1 + num2;
+  console.log(num1 + num2);
+}
+
+
+/*
+  Create a function that subtracts two numbers
+  passed in as arguments. Return the difference.
+ */
+function subtractNum(num1, num2) {
+  return num1 - num2;
+}
+
+/*
+  Create a function that divides two numbers
+  passed in as arguments. Return the quotient.
+ */
+function divideNum(num1, num2) {
+  return num1 / num2;
+}
+
+
+/*
+  Create a function that accepts three arguments.
+    1. First number
+    2. Second number
+    3. A function that performs an operation on them
+
+  Return the value of the operation.
+ */
+function calculateNum(num1, num2, operation) {
+  return operation(num1, num2);
+}
+
+
+/*** Event Listeners ***/
+// addButton.addEventListener("click", calculateNum(firstInput.value, secondInput.value, addNum));
+// subtractButton.addEventListener("click", );
+// multiplyButton.addEventListener("click", );
+// divideButton.addEventListener("click", );
+
+
+document.querySelector("body").addEventListener("click", function(event) {
+  console.log(event);
+  var first = firstInput.value;
+  var second = secondInput.value;
+  var thisElement = event.target;
+  if (thisElement.id === "add") {
+    resultDiv.innerHTML = calculateNum(parseInt(first), parseInt(second), addNum);
+  } else if (thisElement.id === "subtract") {
+    resultDiv.innerHTML = calculateNum(parseInt(first), parseInt(second), subtractNum);
+  } else if (thisElement.id === "multiply") {
+    resultDiv.innerHTML = calculateNum(parseInt(first), parseInt(second), multiplyNum);
+  } else if (thisElement.id === "divide") {
+    resultDiv.innerHTML = calculateNum(parseInt(first), parseInt(second), divideNum);
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
